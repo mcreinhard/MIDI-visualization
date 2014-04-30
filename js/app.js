fcd9b8454eb8644f3ab6b6efe3f83ed72e2152c8
Michael Reinhard <mcreinhard@gmail.com>
2014-04-29 18:54:42 -0700

    Implemented interval recognition and display

--- js/app.js
+++ js/app.js
@@ -7,6 +7,20 @@ var streamIndex= 0;
 var tempoMultiplier = 1;
 var pedal = false;
 
+var intervalColors = {
+  1: "orange",
+  2: "yellow",
+  3: "purple",
+  4: "brown",
+  5: "green",
+  6: "red",
+  7: "green",
+  8: "purple",
+  9: "brown",
+  10: "yellow",
+  11: "orange",
+  12: "blue"
+};
 
 var svg = d3.select("body")
             .append("svg")
@@ -28,17 +42,18 @@ var listenerCallback = function(data) {
     svg.append("circle")
       .attr("cx", note * 10)
       .attr("cy", 50)
-      .attr("r", velocity / 5)
+      .attr("r", velocity / 7)
       .attr("opacity", 1)
       .transition()
       .duration(50)
-      .attr("r", velocity / 3)
+      .attr("r", velocity / 4)
       .transition()
       .duration(2000)
       .ease("sin")
       .attr("cy", height - 50)
       .attr("r", 0)
-      .attr("opacity", 0);
+      .attr("opacity", 0)
+      .remove();
   } else if (type === "noteOff") {
     if (!pedal) MIDI.noteOff(0, note, 0);
   } else if (type === "pedal") {
@@ -63,10 +78,38 @@ var loaderCallback = function(midi) {
 var handleEvents = function() {
   var time = stream[streamIndex].time;
   var events = stream[streamIndex].events;
+
+  var intervals = stream[streamIndex].intervals;
+  if (intervals !== undefined) {
+    for (var i = 0; i < intervals.length; i++) {
+      svg.append("line")
+        .attr({
+          "x1": intervals[i].start * 10,
+          "x2": intervals[i].end * 10,
+          "y1": 50,
+          "y2": 50,
+          "opacity": 1
+        })
+        .style("stroke", intervalColors[intervals[i].size])
+        .transition()
+        .duration(50)
+        .transition()
+        .duration(2000)
+        .ease("sin")
+        .attr({
+          "y1": height - 50,
+          "y2": height - 50,
+          "opacity": 0
+        })
+        .remove();
+    }
+  }
+  
   for (var i = 0; i < events.length; i++) {
     var event = events[i];
     listenerCallback(event);
   }
+  
   streamIndex++;
   setTimeout(handleEvents, (stream[streamIndex].time - time) / (1000 * tempoMultiplier));
 };
